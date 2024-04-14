df9b41189b3199cfce7c20ca3d1337b7640e9c27
Marc McAdoo <marcmcadoo123@gmail.com>
2024-04-14 01:47:40 -0400

    disabled spawning asteroids/enemies until play

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -6,11 +6,15 @@ public class LogicScript : MonoBehaviour
 {
   
   public GameObject welcomeScreen;
+  public GameObject asteroidSpawn;
+  public GameObject enemySpawn;
 
   public void startGame()
   {
    //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
    welcomeScreen.SetActive(false);
+   asteroidSpawn.SetActive(true);
+   enemySpawn.SetActive(true);
   }
 
 }
